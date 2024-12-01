b9a505aa8968531ba800bbcf72f98c1558c2e448
junyonglee0223 <ljyong3339@gmail.com>
2024-12-01 15:09:31 +0900

    5월 식품들의 총매출 조회하기

---
+++ 5월 식품들의 총매출 조회하기.sql
@@ -0,0 +1,6 @@
+select a.PRODUCT_ID, a.PRODUCT_NAME, sum(AMOUNT * PRICE) as TOTAL_SALES 
+from FOOD_PRODUCT a join FOOD_ORDER b
+    on a.PRODUCT_ID = b.PRODUCT_ID
+where PRODUCE_DATE like '2022-05%'
+group by PRODUCT_ID
+order by TOTAL_SALES desc, 
