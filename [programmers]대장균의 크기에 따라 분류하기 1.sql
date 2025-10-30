3be58bbfa631e9cae878806030a1adee07db7a7f
junyonglee0223 <ljyong3339@gmail.com>
2025-10-31 02:43:24 +0900

    [programmers]대장균의 크기에 따라 분류하기 1

---
+++ [programmers]대장균의 크기에 따라 분류하기 1.sql
@@ -0,0 +1,11 @@
+--[programmers]대장균의 크기에 따라 분류하기 1
+
+SELECT 
+    ID,
+    CASE
+        WHEN SIZE_OF_COLONY <= 100 THEN 'LOW'
+        WHEN SIZE_OF_COLONY <= 1000 THEN 'MEDIUM'
+        ELSE 'HIGH'
+    END AS SIZE
+FROM ECOLI_DATA
+ORDER BY ID;
