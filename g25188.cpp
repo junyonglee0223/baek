cfa050f9ec2fe3d979349ece36c5e58e9668b3aa
junyonglee0223 <ljyong3339@gmail.com>
2026-02-12 16:03:50 +0900

    g25188

--- g25188.cpp
+++ g25188.cpp
@@ -7,50 +7,44 @@ typedef long long ll;
 
 int N;
 vector<ll>ARR;
-vector<ll>ARRSUM;
-vector<vector<ll>>DPARR;
 
-ll dfs_arr_v1(int idx, int cnt){
+void solution_one_three_mo_divide_v1(){
 
-    if(cnt >= 6)return 0;
+    const ll NEG = -(1LL << 60);
 
-    ll& ret = DPARR[idx][cnt];
-    if(ret != -1)return ret;
-    ret = 0;
+    auto add = [&](int seg, ll x) -> ll {
+        return (seg % 2 == 0) ? x : 0;
+    };
 
-    if(cnt % 2 == 0){
-        ll cur_last = ARRSUM[N-1] - (idx == 0 ? 0 : ARRSUM[idx-1]);
-        ret = max(ret, cur_last);
-    }
+    vector<ll>dp(6, NEG);
 
-    for(int nxt = idx+1; nxt < N; nxt++){
-        if(cnt % 2 == 0){
-            ll seg_sum = ARRSUM[nxt-1] - (idx == 0 ? 0 : ARRSUM[idx-1]);
-            ll nxt_seg = seg_sum + dfs_arr_v1(nxt, cnt+1);
-            ret = max(ret, nxt_seg);
-        }else{
-            ll nxt_sum = dfs_arr_v1(nxt, cnt+1);
-            ret = max(ret, nxt_sum);
-        }
-    }
-    return ret;
-}
+    ll ret = 0;
+    dp[0] = add(0, ARR[0]);
+    dp[1] = add(1, ARR[0]);
 
+    ret = max(ret, dp[0]);
+    ret = max(ret, dp[1]);
 
-void solution_one_three_mo_divide(){
-    DPARR.resize(N, vector<ll>(6, -1));
-    for(int i = 0; i<N; i++){
-        for(int j = 0; j<6; j++){
-            DPARR[i][j] = -1;
-        }
-    }
-
-    ll ret = dfs_arr_v1(0, 0);
     for(int i = 1; i<N; i++){
-        ret = max(ret, dfs_arr_v1(i, 2));
-    }
+        vector<ll>ndp(6, NEG);
 
-    cout<<ret<<"\n"; 
+        if(dp[0] != NEG) ndp[0] = max(ndp[0], dp[0] + add(0, ARR[i]));
+
+        for(int k = 1; k<6; k++){
+            if(dp[k] != NEG) 
+                ndp[k] = max(ndp[k], dp[k] + add(k, ARR[i]));
+            if(dp[k-1] != NEG) 
+                ndp[k] = max(ndp[k], dp[k-1] + add(k, ARR[i]));
+
+        }
+
+        dp = ndp;
+
+        ret = max(ret, dp[0]);
+        ret = max(ret, dp[2]);
+        ret = max(ret, dp[4]);
+    }
+    cout<<ret<<"\n";
 }
 
 
@@ -64,17 +58,10 @@ int main(){
         cin>>arr[i];
     }
 
-    vector<ll>arrSum(n, 0);
-    arrSum[0] = arr[0];
-    for(int i = 1; i<n; i++){
-        arrSum[i] = arrSum[i-1] + arr[i];
-    }
-
     N = n;
     ARR = arr;
-    ARRSUM = arrSum;
 
-    solution_one_three_mo_divide();
+    solution_one_three_mo_divide_v1();
 
     return 0;
 }
